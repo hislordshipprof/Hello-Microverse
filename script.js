da3c32c959b0e5fae8a07fc6ea4fea229c0ebeac
Anas Abubakar <anasabubakar2015@gmail.com>
2022-06-13 17:45:38 +0100

    Add the funtion to control various toggle button

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const menuClose = document.getElementById('menu-close');
 const menuLinks = document.querySelectorAll('.menu-link');
 const header = document.querySelector('.header-container');
 
-// function toggleMobileMenu(element) {
-//   element.addEventListener('click', () => {
-//     mobileMenu.classList.toggle('display-none');
-//     header.classList.toggle('display-none');
-//     header.classList.toggle('position-fixed');
-//     mainContainer.classList.toggle('no-scroll');
-//   });
-// }
+function toggleMobileMenu(element) {
+  element.addEventListener('click', () => {
+    mobileMenu.classList.toggle('display-none');
+    header.classList.toggle('display-none');
+    header.classList.toggle('position-fixed');
+    mainContainer.classList.toggle('no-scroll');
+  });
+}
 
-// toggleMobileMenu(hamburgerMenu);
-// toggleMobileMenu(menuClose);
+toggleMobileMenu(hamburgerMenu);
+toggleMobileMenu(menuClose);
 
-// menuLinks.forEach((link) => {
-//   toggleMobileMenu(link);
-// });
+menuLinks.forEach((link) => {
+  toggleMobileMenu(link);
+});
